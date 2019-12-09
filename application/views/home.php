95014c68a844ddc6866fcf49ec4f421fdf0e0cba
Paramastri Ardiningrum <paramastriii@gmail.com>
2019-12-09 13:19:16 +0700

    view login & home

--- application/views/home.php
+++ application/views/home.php
@@ -2,7 +2,25 @@
 <link rel="icon" href="<?php echo site_url('asset') ?>/logo.png" type="png" sizes="16x16">
 <html>
 <head>
-  <title>E-Resource Class</title>
+<title>E-Resource Class</title>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
 <style>
 .button {
   background-color: #4CAF50; /* Green */
@@ -24,12 +42,18 @@
 </style>
 </head>
 <body>
+<div style="position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%) ">
 
-<h2>E-Resources</h2>
+<h1 style="text-align: center; margin-bottom: 40px; font-family: Helvetica;">Welcome, <?php echo $this->session->userdata('user_login')['name']; ?>!</h1>
+
 
 <button class="button button4">Jelajahi</button>
 <button class="button button4"><a href="<?php echo base_url('certificate/showSertif') ?>">Sertifikat</a></button>
 <button class="button button4">Skor Test</button>
-
+</div>
 </body>
 </html>
--- application/views/login.php
+++ application/views/login.php
@@ -1,10 +1,44 @@
-<link rel="icon" href="asset/logo.jpg" type="png" sizes="16x16">
+<head>
+    <link rel="icon" href="asset/logo.jpg" type="png" sizes="16x16">
 <title>E-Resource Class</title>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
+
+</head>
+
+
+<link rel="icon" href="asset/logo.jpg" type="png" sizes="16x16">
+
+
+<div style="position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%) ">
+
+    <h1 style="text-align: center; margin-bottom: 40px;">E-Resource</h1>
 
 <form id="login-form" class="js-validation-signin px-30" action="<?php echo base_url('login/dologin') ?>" method="post">
                     <div class="form-group row">
                         <div class="col-12">
-                            <div class="form-material form-material-primary floating">
+                            <div style="width:100%" class="form-material form-material-primary floating">
                                 <label for="loginUsername">Email</label>
                                 <input type="email" class="form-control" id="loginUsername" name="loginUsername">
                             </div>
@@ -12,7 +46,7 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-12">
-                            <div class="form-material form-material-primary floating">
+                            <div style="width: 100%" class="form-material form-material-primary floating">
                                 <label for="loginPassword">Password</label>
                                 <input type="password" class="form-control" id="loginPassword" name="loginPassword">
                             </div>
@@ -27,8 +61,8 @@
                         </div>
                     </div> -->
                     <div class="form-group">
-                        <button type="submit" class="btn btn-sm btn-hero btn-alt-primary">
-                            <i class="si si-login mr-10"></i> Masuk
+                        <button type="submit" class="btn btn-primary">
+                            <i class="si si-login mr-10"></i>Masuk
                         </button>
                         <!-- <div class="mt-30">
                             <a class="link-effect text-muted mr-10 mb-5 d-inline-block" href="op_auth_signup2.html">
@@ -40,3 +74,4 @@
                         </div> -->
                     </div>
                 </form>
+                </div>
