9c77edc605a2f26374f7d743c1c34b8e790706ca
Safhira <safhiramaharani.17051@mhs.its.ac.id>
2019-12-10 20:18:25 +0700

    jurnal

--- application/controllers/journal.php
+++ application/controllers/journal.php
@@ -3,9 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Journal extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('pretest');
+		$this->load->model('posttest');
+		$this->load->model('user');
+		$this->load->library('session');
+	}
+
 	public function index()
 	{
-		
+		$sql = "SELECT * FROM journal;";
+        $data['journal'] = $this->db->query($sql, array())->result();
+		$this->load->view('jurnal', $data);
 	}
 
 }
